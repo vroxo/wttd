91ef3e2c725bff74dd0c174891ae2734bf0c1f87
vroxo <vitor.icnv@gmail.com>
2016-09-22 23:23:52 -0300

    Fix Error encode

--- eventex/subscriptions/views.py
+++ eventex/subscriptions/views.py
@@ -1,6 +1,7 @@
+# coding=utf-8
+
 from django.contrib import messages
 from django.core import mail
-from django.http import HttpResponse
 from django.http import HttpResponseRedirect
 from django.shortcuts import render
 from django.template.loader import render_to_string
