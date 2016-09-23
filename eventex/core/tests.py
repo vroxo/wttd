97492f3f3e0e348d75a0fdae4081e6b9e504e3d4
vroxo <vitor.icnv@gmail.com>
2016-09-22 23:34:36 -0300

    Link Inscrição

--- eventex/core/tests.py
+++ eventex/core/tests.py
@@ -13,3 +13,6 @@ class HomeTest(TestCase):
     def test_template(self):
         """Must use index.html"""
         self.assertTemplateUsed(self.response, 'index.html')
+
+    def test_link_subscription(self):
+        self.assertContains(self.response, 'href="/inscricao/"')
