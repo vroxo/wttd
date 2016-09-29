0a70b611a9b9bfa1e70caf2434899c3ce39e9792
vroxo <vitor.icnv@gmail.com>
2016-09-28 21:46:29 -0300

    Admin magic

---
+++ eventex/subscriptions/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class SubscriptionsAppConfig(AppConfig):
+    name = 'eventex.subscriptions'
+    verbose_name = 'Controle de Participantes'
--- eventex/subscriptions/tests/test_model_subscription.py
+++ eventex/subscriptions/tests/test_model_subscription.py
@@ -21,3 +21,6 @@ class SubscriptionModelTest(TestCase):
 
     def test_created_at(self):
         self.assertIsInstance(self.obj.created_at, datetime)
+
+    def test_str(self):
+        self.assertEqual('Vitor', str(self.obj))
