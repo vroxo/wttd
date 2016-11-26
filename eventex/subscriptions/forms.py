d914e8b947a9a12d28bcd6c2c7786c8fc49e873e
vroxo <vitor.icnv@gmail.com>
2016-11-26 03:55:36 -0200

    Fix encoding

--- eventex/subscriptions/forms.py
+++ eventex/subscriptions/forms.py
@@ -1,3 +1,4 @@
+# coding=utf-8
 from django import forms
 from django.core.exceptions import ValidationError
 
@@ -7,7 +8,7 @@ def validate_cpf(value):
         raise ValidationError('CPF deve conter apenas números.', 'digits')
 
     if len(value) != 11:
-        raise  ValidationError('CPF deve ter 11 números.', 'length')
+        raise ValidationError('CPF deve ter 11 números.', 'length')
 
 
 class SubscriptionForm(forms.Form):
