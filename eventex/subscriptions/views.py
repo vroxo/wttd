02e7a37d659ce863704eb6c8e37f4e69656479bd
vroxo <vitor.icnv@gmail.com>
2016-12-12 13:09:21 -0200

    Fix views.py

--- eventex/subscriptions/views.py
+++ eventex/subscriptions/views.py
@@ -26,7 +26,7 @@ def create(request):
     form = SubscriptionForm(request.POST)
 
     if not form.is_valid():
-        return render(request, 'subscriptions/subscription_form.html', {'form': SubscriptionForm()})
+        return render(request, 'subscriptions/subscription_form.html', {'form': form})
 
     subscription = Subscription.objects.create(**form.cleaned_data)
 
